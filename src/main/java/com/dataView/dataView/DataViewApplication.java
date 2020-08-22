13d01d3c2d6b7adc8ef47fc8442beb65997067a6
SushilMallMobikwiks <sushil.mall@mobikwik.com>
2020-08-22 18:25:54 +0530

    Test a cron

--- src/main/java/com/dataView/dataView/DataViewApplication.java
+++ src/main/java/com/dataView/dataView/DataViewApplication.java
@@ -2,8 +2,10 @@ package com.dataView.dataView;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DataViewApplication {
 
 	public static void main(String[] args) {
